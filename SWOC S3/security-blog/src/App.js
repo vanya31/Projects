6751a7e1def875fd952e4a698e6c7b904463c8bb
Vanya Verma <72427243+vanya31@users.noreply.github.com>
2023-01-13 10:17:20 +0530

    Update

--- SWOC S3/security-blog/src/App.js
+++ SWOC S3/security-blog/src/App.js
@@ -2,12 +2,17 @@ import React from "react";
 import "./App.css";
 
 import Posts from "./components/Posts/Posts";
+import Dashboard from "./components/Dashboard/Dashboard";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+
+
 
 const App = () => {
 return (
 	<div className="main-container">
 	<h1 className="main-heading">
-		Blog App using React Js
+		Technical Blog
 	</h1>
 	<Posts />
 	</div>
